a919a3198b6285146a599556a624794aa3e9f902
Josué Tille <josue@tille.ch>
2017-07-21 15:00:56 +0200

    Full app Update

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,122 +1,17 @@
 #!/bin/bash
 
-## Adapt md5sum while you update app
-md5sum="e561dda5a9cc1f5c2dcef011cabff725"
-riot_version="0.11.4"
+# Retrieve arguments
+app=$YNH_APP_INSTANCE_NAME
+final_path="/var/www/$app"
 
-init_script() {
-    # Exit on command errors and treat unset variables as an error
-    set -eu
+config_nginx() {
+	cp ../conf/nginx.conf /etc/nginx/conf.d/$domain.d/$app.conf
 
-    # Source YunoHost helpers
-    source /usr/share/yunohost/helpers
-
-    # Retrieve arguments
-    app=$YNH_APP_INSTANCE_NAME
-    CHECK_VAR "$app" "app name not set"
+	ynh_replace_string __PATH__ $path /etc/nginx/conf.d/$domain.d/$app.conf
+	ynh_replace_string __FINALPATH__ $final_path /etc/nginx/conf.d/$domain.d/$app.conf
 }
 
-get_source() {
-
-    wget -q -O '/tmp/riot.tar.gz' "https://github.com/vector-im/riot-web/releases/download/v${riot_version}/riot-v${riot_version}.tar.gz"
-
-    if [[ ! -e '/tmp/riot.tar.gz' ]] || [[ $(md5sum '/tmp/riot.tar.gz' | cut -d' ' -f1) != $md5sum ]]
-    then
-        ynh_die "Error : can't get Riot source"
-    fi
-    tar xzf '/tmp/riot.tar.gz'
-    sudo cp -r riot-v${riot_version}/. $final_path/
-    sudo chown www-data -R $final_path
-    sudo chmod 740 -R $final_path
-}
-
-
-CHECK_VAR () {	# Vérifie que la variable n'est pas vide.
-# $1 = Variable à vérifier
-# $2 = Texte à afficher en cas d'erreur
-	test -n "$1" || (echo "$2" >&2 && false)
-}
-
-# Ignore the yunohost-cli log to prevent errors with conditionals commands
-# usage: NO_LOG COMMAND
-# Simply duplicate the log, execute the yunohost command and replace the log without the result of this command
-# It's a very badly hack...
-# Petite copie perso à mon usage ;)
-NO_LOG() {
-  ynh_cli_log=/var/log/yunohost/yunohost-cli.log
-  sudo cp -a ${ynh_cli_log} ${ynh_cli_log}-move
-  eval $@
-  exit_code=$?
-  sudo mv ${ynh_cli_log}-move ${ynh_cli_log}
-  return $?
-}
-
-CHECK_USER () {	# Vérifie la validité de l'user admin
-# $1 = Variable de l'user admin.
-	ynh_user_exists "$1" || (echo "Wrong admin" >&2 && false)
-}
-
-CHECK_PATH () {	# Vérifie la présence du / en début de path. Et son absence à la fin.
-	if [ "${path:0:1}" != "/" ]; then    # Si le premier caractère n'est pas un /
-		path="/$path"    # Ajoute un / en début de path
-	fi
-	if [ "${path:${#path}-1}" == "/" ] && [ ${#path} -gt 1 ]; then    # Si le dernier caractère est un / et que ce n'est pas le seul caractère.
-		path="${path:0:${#path}-1}"	# Supprime le dernier caractère
-	fi
-}
-
-CHECK_FINALPATH () {	# Vérifie que le dossier de destination n'est pas déjà utilisé.
-	final_path=/var/www/$app
-	if [ -e "$final_path" ]
-	then
-		echo "This path already contains a folder" >&2
-		false
-	fi
-}
-
-FIND_PORT () {	# Cherche un port libre.
-# $1 = Numéro de port pour débuter la recherche.
-	port=$1
-	while ! sudo yunohost app checkport $port ; do
-		port=$((port+1))
-	done
-	CHECK_VAR "$port" "port empty"
-}
-
-### REMOVE SCRIPT
-
-REMOVE_NGINX_CONF () {	# Suppression de la configuration nginx
-	if [ -e "/etc/nginx/conf.d/$domain.d/$app.conf" ]; then	# Delete nginx config
-		echo "Delete nginx config"
-		sudo rm "/etc/nginx/conf.d/$domain.d/$app.conf"
-		sudo service nginx reload
-	fi
-}
-
-SECURE_REMOVE () {      # Suppression de dossier avec vérification des variables
-	chaine="$1"	# L'argument doit être donné entre quotes simple '', pour éviter d'interpréter les variables.
-	no_var=0
-	while (echo "$chaine" | grep -q '\$')	# Boucle tant qu'il y a des $ dans la chaine
-	do
-		no_var=1
-		global_var=$(echo "$chaine" | cut -d '$' -f 2)	# Isole la première variable trouvée.
-		only_var=\$$(expr "$global_var" : '\([A-Za-z0-9_]*\)')	# Isole complètement la variable en ajoutant le $ au début et en gardant uniquement le nom de la variable. Se débarrasse surtout du / et d'un éventuel chemin derrière.
-		real_var=$(eval "echo ${only_var}")		# `eval "echo ${var}` permet d'interpréter une variable contenue dans une variable.
-		if test -z "$real_var" || [ "$real_var" = "/" ]; then
-			echo "Variable $only_var is empty, suppression of $chaine cancelled." >&2
-			return 1
-		fi
-		chaine=$(echo "$chaine" | sed "s@$only_var@$real_var@")	# remplace la variable par sa valeur dans la chaine.
-	done
-	if [ "$no_var" -eq 1 ]
-	then
-		if [ -e "$chaine" ]; then
-			echo "Delete directory $chaine"
-			sudo rm -r "$chaine"
-		fi
-		return 0
-	else
-		echo "No detected variable." >&2
-		return 1
-	fi
+config_riot() {
+	cp ../conf/config.json $final_path/config.json
+	ynh_replace_string __DEFAULT_SERVER__ $default_home_server $final_path/config.json
 }
